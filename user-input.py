f3cea7056d5b3dbf3583cc3519ec945c32d09147
grum-sudo <grum_@tuta.io>
2026-03-13 17:33:07 +0300

    user input

---
+++ user-input.py
@@ -0,0 +1,17 @@
+name = input("what is your name?: ")
+age = int(input ("how old are you?: "))
+
+age = age + 1
+
+print(f"hello {name}")
+print("happy birthday")
+print(f"you are {age} years old")
+
+#exercise 1
+
+length = float(input("what is the length?:"))
+width = float(input ("what is the width?:"))
+
+area = length * width
+
+print(f"the area is {area}")
