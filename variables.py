824758cb533c4e5bd0081c817fc92f316887ed35
grum-sudo <grum_@tuta.io>
2026-03-13 16:57:55 +0300

    variables

---
+++ variables.py
@@ -0,0 +1,29 @@
+#strings
+first_name = "grum"
+food = "pizza"
+
+print(f"hello {first_name}")
+print(f"you like {food}")
+
+#integers
+age = 17 
+quantity = 5
+
+print(f"you are {age} years old")
+print(f"you are buying {quantity} items")
+
+#float
+
+price = 2.5
+gpa = 3.8
+
+print(f"the price is {price}$")
+print(f"your gpa is {gpa}")
+
+#boolean
+is_student = False 
+ 
+if is_student:
+    print("you are a student")
+else: 
+    print("you are not a student")
